83f82929947abbaec3a99c5ba51fa143767bf630
BogdanCatalin-Iacob <bogdancatalin.iacob@gmail.com>
2023-10-29 21:46:39 +0000

    Create symbol check function

--- main.py
+++ main.py
@@ -2,6 +2,7 @@
 Generate password containing Uppercase letters,
 Lowercase letters and symbols
 '''
+import string
 
 
 def contains_upper(password: str) -> bool:
@@ -10,5 +11,15 @@ def contains_upper(password: str) -> bool:
     '''
     for char in password:
         if char.isupper():
+            return True
+    return False
+
+
+def contains_symbols(password: str) -> bool:
+    '''
+    Check if the password contains symbols
+    '''
+    for char in password:
+        if char in string.punctuation:
             return True
     return False
